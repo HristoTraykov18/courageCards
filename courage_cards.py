96694db0dc6e3e94e0e7093409155cae1c86c814
Hristo Traykov <hristotraykov98@gmail.com>
2023-10-25 11:36:28 +0300

    Completed validation

--- courage_cards.py
+++ courage_cards.py
@@ -27,10 +27,10 @@ def contains_valid_data(df):
               'red_card', 'banked', 'end')
 
     if 'event' not in df.columns or 'created' not in df.columns:
-        print('[ERROR] Could not find \'event\' or \'created\' columns!')
+        print('[ERROR] Could not find \'event\' and/or \'created\' columns!')
         return False
 
-    df_events = df[df.columns[0]]
+    df_events = df['event']
 
     # Validate start and end events
     if df_events.iloc[0] != events[0] or df_events.iloc[-1] != events[-1]:
@@ -46,7 +46,22 @@ def contains_valid_data(df):
             print('[ERROR] \'start\' or \'end\' events used more than once!')
             return False
 
-    df_created = df[df.columns[0]]
+    df_created = df['created']
+
+    try:
+        pd.to_datetime(df_created, format='%Y-%m-%d %H:%M:%S.%f-07:00')
+    except:
+        print(f"[ERROR] Invalid datatime format provided!")
+        return False
+
+    df_len = len(df_created)
+
+    for i in range(0, df_len - 1):
+        if df_created.iloc[i] >= df_created.iloc[i+1]:
+            print(
+                '[ERROR] Event {} is created earlier than event {}, while it should be the other way around!'.format(
+                    df_created.keys()[i+1], df_created.keys()[i]))
+            return False
 
     return True
 
@@ -70,6 +85,5 @@ if __name__ == '__main__':
     if (not contains_valid_data(df)):
         exit(code=2)
 
-    # print(df['event'].index[0])
-    # courage_cards = CourageCards(df)
-    # courage_cards.create_output_csv()
+    courage_cards = CourageCards(df)
+    courage_cards.create_output_csv()
